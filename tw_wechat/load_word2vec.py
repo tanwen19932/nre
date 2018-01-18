820afdd0a82dc345971c1ae98b521183f0b4ecc2
tanwen19932 <tanwen19932@163.com>
2018-01-18 17:59:55 +0800

    增加内容read_csv，小部分词向量代码

--- tw_wechat/load_word2vec.py
+++ tw_wechat/load_word2vec.py
@@ -3,6 +3,7 @@ import os
 import gensim
 import numpy as np
 import pandas as pd
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 
 print('Indexing word vecto')
 from keras.layers import Conv1D, MaxPooling1D, Embedding
@@ -14,7 +15,7 @@ from keras.utils import to_categorical
 
 
 MAX_SEQUENCE_LENGTH = 100 # 每篇句子选取1000个词
-MAX_NB_WORDS = 10000 # 将字典设置为含有1万个词
+MAX_NB_WORDS = 20000 # 将字典设置为含有1万个词
 EMBEDDING_DIM = 300 # 词向量维度，300维
 VALIDATION_SPLIT = 0.2 # 测试集大小，全部数据的20%
 
@@ -101,24 +102,24 @@ for word, i in word_index.items():
         # 文本数据中的词在词向量字典中没有，向量为取0；如果有则取词向量中该词的向量
         embedding_matrix[i] = embedding_vector
 
-embedding_layer = Embedding(num_words,
-                            EMBEDDING_DIM,
-                            weights=[embedding_matrix],
+embedding_layer = Embedding(num_words,#词个数
+                            EMBEDDING_DIM,#300维向量
+                            weights=[embedding_matrix],#向量矩阵
                             input_length=MAX_SEQUENCE_LENGTH,
                             trainable=False)
 
 
-sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = embedding_layer(sequence_input)
-x = Conv1D(filters=128, kernel_size=5, activation='relu')(embedded_sequences)
-x = MaxPooling1D(pool_size=5)(x)
-x = Conv1D(filters=128, kernel_size=5, activation='relu')(x)
-x = MaxPooling1D(pool_size=5)(x)
-x = Conv1D(filters=128, kernel_size=5, activation='relu')(x)
-x = MaxPooling1D(pool_size=35)(x)
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')# 100*1最多100个词组成输入
+embedded_sequences = embedding_layer(sequence_input)# 句子转为向量矩阵100*300维
+x = Conv1D(filters=128, kernel_size=5, activation='relu')(embedded_sequences)#成为96*296*128
+x = MaxPooling1D(pool_size=5)(x)#成为92*292*128
+x = Conv1D(filters=128, kernel_size=5, activation='relu')(x)#成为88*288*128
+x = MaxPooling1D(pool_size=5)(x)#84*284*128
+x = Conv1D(filters=128, kernel_size=5, activation='relu')(x)#80*280*128
+x = MaxPooling1D(pool_size=35)(x)#46*246*128
 x = Flatten()(x)
-x = Dense(128, activation='relu')(x)
-preds = Dense(len(labels_index), activation='softmax')(x)
+x = Dense(128, activation='relu')(x)#128全连接
+preds = Dense(len(labels_index), activation='softmax')(x)#softmax分类
 
 model = Model(sequence_input, preds)
 model.compile(loss='categorical_crossentropy',
@@ -126,9 +127,22 @@ model.compile(loss='categorical_crossentropy',
               metrics=['acc'])
 
 # 如果希望短一些时间可以，epochs调小
+
+# ModelCheckpoint回调函数将在每个epoch后保存模型到filepath，当save_best_only=True保存验证集误差最小的参数
+file_path="weights_base.best.hdf5"
+checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+# 当监测值不再改善时，该回调函数将中止训练
+early = EarlyStopping(monitor="val_loss", mode="min", patience=20)
+
+# 开始训练
+callbacks_list = [checkpoint, early] #early
+
 model.fit(x_train, y_train,
           batch_size=128,
           epochs=50,
-          validation_data=(x_val, y_val))
+          validation_data=(x_val, y_val),
+          callbacks=callbacks_list)
+
+model.save(filepath="model1.model")
 
 
---
+++ tw_wechat/needword.py
@@ -0,0 +1,38 @@
+import gensim
+import os
+import pandas as pd
+from keras.preprocessing import text, sequence
+# 对文本中的词进行统计计数，生成文档词典，以支持基于词典位序生成文本的向量表示，超过max_features的单词被丢掉
+# 使用一系列文档来生成token词典，texts为list类，每个元素为一个文档
+train = pd.read_csv(filepath_or_buffer="../data/train.txt",delimiter='|',
+                    # header=["type","e1","e2","doc"],
+                    names=["type","e1","e2","doc"]
+                    )
+test = pd.read_csv(filepath_or_buffer="../data/test.txt",delimiter='|')
+
+print(train.columns)
+list_sentences_train = train['doc'].fillna("CVxTz").values
+train_type = train['type'].values
+list_sentences_test = train['doc'].fillna("CVxTz").values
+
+
+need_word2vec = {}
+tokenizer = text.Tokenizer(num_words=100)
+tokenizer.fit_on_texts(list(list_sentences_train))
+word_vec = gensim.models.KeyedVectors.load_word2vec_format(os.path.join(os.path.dirname(__file__), '../data/word2vec/GoogleNews-vectors-negative300.bin'), binary=True)
+
+i = 0
+for word in tokenizer.word_index:
+    need_word2vec[i]=word_vec[word]
+    i+=1
+del word_vec
+
+
+print(need_word2vec)
+
+list_tokenized_train = tokenizer.texts_to_sequences(list_sentences_train)
+list_tokenized_test = tokenizer.texts_to_sequences(list_sentences_test)
+
+x_train = sequence.pad_sequences(list_tokenized_train, maxlen=100)
+x_test = sequence.pad_sequences(list_tokenized_test, maxlen=100)
+
